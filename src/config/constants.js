f12ff002530f1ba481fd82957f00ec381884c859
AwardOne <123alba@naver.com>
2021-05-12 17:42:37 +0900

    api 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "https://rkdgkflfk21.herokuapp.com/";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://rkdgkflfk21.herokuapp.com"
+    : "http://localhost:8080";
